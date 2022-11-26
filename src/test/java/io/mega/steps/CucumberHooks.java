0337e25e6508d9669b3e6dd77592a984e927fb2b
charakaw <mininuwan@gmail.com>
2022-11-26 07:18:33 +0530

    first sample test without errors allure report integrated

--- src/test/java/io/mega/steps/CucumberHooks.java
+++ src/test/java/io/mega/steps/CucumberHooks.java
@@ -3,13 +3,19 @@ package io.mega.steps;
 import io.mega.webdriver.WebDriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.ByteArrayInputStream;
 
 public class CucumberHooks {
     /**
      * Runs before each test(feature) with tag '@ui'
      */
     @Before("@ui")
-    public void beforeEach(){
+    public void beforeEach() throws Exception{
         WebDriverFactory.createDriver();
     }
 
@@ -17,7 +23,13 @@ public class CucumberHooks {
      * Runs after each test(features) with tag '@ui' whether pass or fail
      */
     @After("@ui")
-    public void afterEach() {
+    public void afterEach(Scenario scenario) {
+        // To take screenshot and add it to Allure report if test fails
+        if (scenario.isFailed()) {
+            Allure.addAttachment(scenario.getName(), "image/png", new ByteArrayInputStream(
+                    ((TakesScreenshot) WebDriverFactory.getDriver()).getScreenshotAs(OutputType.BYTES)), "png");
+        }
+
         WebDriverFactory.cleanUpDriver();
     }
 }
